dc91704eb8adf113986fd0596235fe2fa42723c3
tanp@corp.21cn.com <tanp@corp.21cn.com>
2021-04-08 20:57:09 +0800

    jdk singleton implementation

---
+++ src/main/java/com/tanp/designpattern/singleton/Test.java
@@ -0,0 +1,13 @@
+package com.tanp.designpattern.singleton;
+
+/**
+ * @author CodeBricklayer
+ * @date 2021/4/8 20:51
+ * @description java内部的单例模式
+ */
+public class Test {
+    public static void main(String[] args) {
+        //采用的是饿汉式
+        Runtime runtime = Runtime.getRuntime();
+    }
+}
